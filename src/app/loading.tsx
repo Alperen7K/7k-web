db7bbbb9b7fdc27a575ef1dcc49d266064011140
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-07-14 12:29:56 +0300

    NotFound and Loading added

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+ return (
+  <div className="w-full h-screen flex items-center justify-center ">
+   <h1 className="text-[40px] text-white animate-pulse">{"7K's"}</h1>
+  </div>
+ );
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+import home from "../../public/icons/home.svg";
+
+export default function NotFound() {
+ return (
+  <div className="bg-black flex flex-col items-center justify-center gap-y-[30vh] pb-[10vh] sm:pb-0 text-white w-full h-screen">
+   <div>
+    <h1 className="text-[100px] font-bold  text-center">404</h1>
+    <h1 className="text-[40px] font-bold  text-center">Page Not Found</h1>
+   </div>
+
+   <a href="/" rel="Home Link" className=" ">
+    <Image
+     src={home}
+     alt="Home icon"
+     className="size-20"
+     width={40}
+     height={40}
+    />
+   </a>
+  </div>
+ );
+}
