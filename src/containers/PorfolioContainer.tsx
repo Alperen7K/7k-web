156b861541d09c2c9f7d52d977e423ce891d5e4a
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-07-14 12:39:23 +0300

    key prop added

--- src/containers/PorfolioContainer.tsx
+++ src/containers/PorfolioContainer.tsx
@@ -5,8 +5,8 @@ import React from "react";
 export default function PorfolioContainer() {
  return (
   <div className="mx-auto max-w-[800px] space-y-4 overflow-x-hidden ">
-   {PortfolioData.map((portfolio: PortfolioDataType) => {
-    return <PortfolioCard data={portfolio} />;
+   {PortfolioData.map((portfolio: PortfolioDataType, index: number) => {
+    return <PortfolioCard key={index} data={portfolio} />;
    })}
   </div>
  );
