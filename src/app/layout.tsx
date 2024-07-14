5bf1d66b89d2c9741111636a216968d326de78f4
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-07-14 12:31:11 +0300

    layout done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,33 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
  title: "Mehmet Alperen YEDİK",
+ authors: [
+  { name: "Mehmet Alperen YEDİK", url: "https://github.com/Alperen7K" },
+ ],
  description: "owner Mehmet Alperen YEDİK",
+ icons: "./facion.ico",
 };
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function RootLayout({
  children,
- navbar,
- footer,
 }: Readonly<{
  children: React.ReactNode;
- navbar: React.ReactNode;
- footer: React.ReactNode;
 }>) {
  return (
   <html lang="en">
    <body className={montserrat.className}>
-    {navbar}
+    <Navbar />
     <div className="min-h-screen w-full overflow-x-hidden bg-black  text-white">
      {children}
     </div>
-    {footer}
+    <Footer />
    </body>
   </html>
  );
