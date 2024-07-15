da0e7550bfdffb2c693a47d82bf196b9bed02057
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-07-15 11:48:14 +0300

    techs section changed to hidden to md size

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -19,9 +19,9 @@ export default function PortfolioCard({
     navigate.push("/portfolio/" + data.path);
    }}
    key={key}
-   className="cursor-pointer w-full max-w-[900px] bg-white/5 border-2 border-solid border-white/15 shadow-lg shadow-white/10 rounded-lg flex flex-col justify-between"
+   className="cursor-pointer w-full max-w-[900px] bg-white/5 border-2 border-solid border-white/40 shadow-lg shadow-white/10 rounded-lg flex flex-col justify-between"
   >
-   <div className="px-2 pt-2 pb-1.5 flex flex-col sm:flex-row gap-x-3">
+   <div className="px-2 pt-2 pb-1.5 flex flex-col sm:flex-row gap-3">
     <Image
      src={data.photos[0]}
      alt={data.name + "image"}
@@ -33,7 +33,7 @@ export default function PortfolioCard({
      <h2 className="text-end text-[14px] opacity-80">{data.duty}</h2>
      <h1 className="font-bold line-clamp-1 text-[20px]">{data.name}</h1>
      <p className="text-[16px] opacity-85 line-clamp-4">{data.detail}</p>
-     <div className="flex justify-end flex-nowrap overflow-x-scroll gap-x-1 ">
+     <div className="hidden md:flex justify-end flex-nowrap overflow-x-hidden gap-x-1 ">
       {data.techs.map((tech: string, index: number) => {
        return <TechnologyCard key={index} tech={tech} />;
       })}
