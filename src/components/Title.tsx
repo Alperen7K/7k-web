a6c19906c3cfce6213ae6f2285598a9445c8ae39
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-07-15 13:37:12 +0300

    text-center added to sm

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
 export function Title({ title }: { title: string }) {
  return (
-  <h1 className="w-full sm:text-[52px] text-[35px] font-bold mt-0 pt-0 ">
+  <h1 className="text-center sm:text-start w-full sm:text-[52px] text-[35px] font-bold mt-0 pt-0 ">
    {title}
   </h1>
  );
