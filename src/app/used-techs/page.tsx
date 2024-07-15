37a1331a2e6c7a56faad5769d4856920fde13b17
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-07-15 13:36:49 +0300

    UsedTechs page added

---
+++ src/app/used-techs/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Title } from "@/components/Title";
+import { UsedTechsCard } from "@/components/UsedTechsCard";
+import { UsedTechsData, UsedTechsDataType } from "@/models/used.tech.data";
+
+export default function UsedTechs() {
+ return (
+  <div className="mx-auto  w-[95vw] sm:w-[85vw]  space-y-12  pt-10 pb-10">
+   <Title title="Used Technologies" />
+   <div className="w-full grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 place-items-center gap-2 sm:gap-6 ">
+    {UsedTechsData.map((tech: UsedTechsDataType) => {
+     return <UsedTechsCard img={tech.img} name={tech.name} link={tech.link} />;
+    })}
+   </div>
+  </div>
+ );
+}
