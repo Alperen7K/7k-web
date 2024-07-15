94015a151a727cb41616df5d345f9014963192d4
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-07-15 11:46:55 +0300

    changed space-y

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,7 +4,7 @@ import { Title } from "@/components/Title";
 
 export default function Portfolio() {
  return (
-  <div className="mx-auto  w-[95vw] sm:w-[85vw]  space-y-16  pt-10 pb-20">
+  <div className="mx-auto  w-[95vw] sm:w-[85vw]  space-y-12  pt-10 pb-20">
    <Title title="Porfolio" />
    <PorfolioContainer />
   </div>
