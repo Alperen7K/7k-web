b31d3784d5d56fbd1beb6b1215594b30dc0c83f1
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-07-15 11:46:42 +0300

    font-bold added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Loading() {
  return (
   <div className="w-full h-screen flex items-center justify-center ">
-   <h1 className="text-[40px] text-white animate-pulse">{"7K's"}</h1>
+   <h1 className="text-[40px] text-white font-bold animate-pulse">{"7K's"}</h1>
   </div>
  );
 }
