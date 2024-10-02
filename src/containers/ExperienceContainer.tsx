39b776b8bc01a616a9276ba440e0ca7a23feede1
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-10-02 16:24:19 +0300

    changed sort

--- src/containers/ExperienceContainer.tsx
+++ src/containers/ExperienceContainer.tsx
@@ -13,9 +13,9 @@ export const ExperienceContainer = () => {
        <h1 className=" text-[35px] sm:text-[40px] font-bold  ">{exp.type}</h1>
        <h1 className="opacity-80 text-[16px] ">{exp.date}</h1>
        <div className=" flex flex-col sm:flex-row sm:items-center gap-x-4 text-nowrap">
-        <h1 className=" text-[24px] font-semibold ">{exp.jobTitle}</h1>
-        <span className="hidden sm:block px-4"> - </span>
         <h1 className="w-full  text-[20px]">{exp.company}</h1>
+        <span className="hidden sm:block px-4"> - </span>
+        <h1 className=" text-[24px] font-semibold ">{exp.jobTitle}</h1>
        </div>
        <p className="text-[16px] text-justify opacity-90">{exp.description}</p>
       </div>
