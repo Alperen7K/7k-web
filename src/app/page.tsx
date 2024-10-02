f81331699f992bf869b1b764dfefb22ee8e58537
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-10-02 20:12:11 +0300

    mobile width changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
  return (
   <>
    <Landing />
-   <div className="mx-auto  w-[95vw] sm:w-[85vw]  space-y-16 pb-20">
+   <div className="mx-auto  w-[90vw] sm:w-[85vw]  space-y-16 pb-20">
     <About />
     <Education />
     <ExperienceContainer />
