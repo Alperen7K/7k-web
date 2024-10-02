772d0b068ca2f8f8c1e217b660de61012c472d18
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-10-02 16:18:44 +0300

    Css update

--- src/containers/ExperienceContainer.tsx
+++ src/containers/ExperienceContainer.tsx
@@ -12,7 +12,7 @@ export const ExperienceContainer = () => {
       <div key={index} className="space-y-4  ">
        <h1 className=" text-[35px] sm:text-[40px] font-bold  ">{exp.type}</h1>
        <h1 className="opacity-80 text-[16px] ">{exp.date}</h1>
-       <div className=" flex flex-col sm:flex-row sm:items-center gap-x-4 *:sm:text-nowrap">
+       <div className=" flex flex-col sm:flex-row sm:items-center gap-x-4 text-nowrap">
         <h1 className=" text-[24px] font-semibold ">{exp.jobTitle}</h1>
         <span className="hidden sm:block">-</span>
         <h1 className="w-full  text-[20px]">{exp.company}</h1>
