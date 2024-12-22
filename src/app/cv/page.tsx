a00330c4d95d52445b3619d6d6ae4455aad2dba8
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2024-12-22 14:32:27 +0300

    cv page restored

--- src/app/cv/page.tsx
+++ src/app/cv/page.tsx
@@ -1,6 +1,26 @@
+"use client";
 import React from "react";
-import PdfViewer from "@/components/PdfViewer";
-// import asd from "../../../public/cv/cv.pdf";
+import { Button, Tooltip } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+
 export default function CV() {
- return <PdfViewer fileUrl="/cv/cv.pdf" />;
+ return (
+  <div className="fixed inset-0 z-[1000]">
+   <Tooltip title="Geri Dön">
+    <Button
+     onClick={() => window.history.back()}
+     className="fixed left-4 top-4 "
+     icon={<ArrowLeftOutlined />}
+     type="default"
+     size="large"
+    />
+   </Tooltip>
+   <iframe
+    loading="lazy"
+    className="w-full min-h-screen"
+    src="https://www.canva.com/design/DAGZwdiGFQ0/fhcuIeEveNUbVRAV5uEbTw/view?embed"
+    allow="fullscreen"
+   ></iframe>
+  </div>
+ );
 }
