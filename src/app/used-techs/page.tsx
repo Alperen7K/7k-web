60c98449826ccc682598a3ecdfb26dc923985672
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2025-03-22 22:11:04 +0300

    UsedTechs bileşeninde stil güncellemeleri yapıldı. Kartların maksimum genişliği artırıldı ve düzen iyileştirildi.

--- src/app/used-techs/page.tsx
+++ src/app/used-techs/page.tsx
@@ -7,7 +7,7 @@ export default function UsedTechs() {
  return (
   <div className="mx-auto  w-[95vw] sm:w-[85vw]  space-y-12  pt-28 pb-10">
    <Title title="Used Technologies" />
-   <div className="w-full grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 place-items-center gap-2 sm:gap-6 ">
+   <div className="xl:max-w-[1200px] 2xl:max-w-[1400px] w-full mx-auto grid grid-cols-3 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 place-items-center gap-2 sm:gap-6 ">
     {UsedTechsData.map((tech: UsedTechsDataType, index: number) => {
      return (
       <UsedTechsCard
