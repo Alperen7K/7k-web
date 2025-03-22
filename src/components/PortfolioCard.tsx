f6c53d83482cc13d60c1402f6418ea333314ce2e
Mehmet Alperen Yedik <121613095+Alperen7K@users.noreply.github.com>
2025-03-22 22:08:34 +0300

    PortfolioCard ve PorfolioContainer bileşenlerinde stil güncellemeleri yapıldı. Kartların maksimum genişliği artırıldı, içerik düzeni ve görsel kalitesi iyileştirildi.

--- src/components/PortfolioCard.tsx
+++ src/components/PortfolioCard.tsx
@@ -20,25 +20,27 @@ export default function PortfolioCard({
     navigate.push("/portfolio/" + data.path);
    }}
    key={key}
-   className="cursor-pointer w-full max-w-[900px] bg-white/5 border-2 border-solid border-white/40 shadow-lg shadow-white/10 rounded-lg flex flex-col justify-between"
+   className="cursor-pointer w-full 2xl:max-w-[1200px] xl:max-w-[900px] bg-white/5 border-2 border-solid border-white/40 shadow-lg shadow-white/10 rounded-2xl flex flex-col justify-between"
   >
-   <div className="px-2 pt-2 pb-1.5 flex flex-col sm:flex-row gap-3 min-w-[200px] min-h-[100px]">
+   <div className="p-2 flex flex-col sm:flex-row gap-3 min-w-[200px] min-h-[100px]">
     <Image
      src={data.photos[0]}
      alt={data.name + "image"}
      width={200}
      loading="lazy"
-     quality={50}
+     quality={70}
      height={100}
-     className="w-full sm:w-[30%] aspect-[5.5/3] object-contain rounded-lg"
+     className="w-full sm:w-[40%] aspect-[5/3] object-cover object-center rounded-lg "
     />
-    <div className="w-full space-y-2 ">
-     <section className="  flex items-center justify-end gap-x-2">
-      {data.link && <LinkCard link={data.link} />}
-      <h2 className="text-[14px] opacity-80 pt-0.5">{data.duty}</h2>
-     </section>
-     <h1 className="font-bold line-clamp-1 text-[20px]">{data.name}</h1>
-     <p className="text-[16px] opacity-85 line-clamp-4">{data.detail}</p>
+    <div className="w-full space-y-2 flex flex-col justify-between">
+     <>
+      <section className="  flex items-center justify-end gap-x-2 pr-1">
+       {data.link && <LinkCard link={data.link} />}
+       <h2 className="text-[14px] opacity-80 pt-0.5">{data.duty}</h2>
+      </section>
+      <h1 className="font-bold line-clamp-1 text-[20px]">{data.name}</h1>
+     </>
+     <p className="text-[16px] opacity-85 line-clamp-3">{data.detail}</p>
      <div className="hidden md:flex justify-end flex-nowrap overflow-x-hidden gap-x-1 ">
       {data.techs.map((tech: string, index: number) => {
        return <TechnologyCard key={index} tech={tech} />;
--- src/containers/PorfolioContainer.tsx
+++ src/containers/PorfolioContainer.tsx
@@ -4,7 +4,7 @@ import { PortfolioData, PortfolioDataType } from "@/models/portfolio";
 
 export default function PorfolioContainer() {
  return (
-  <div className="mx-auto max-w-[900px] space-y-6 overflow-x-hidden ">
+  <div className="mx-auto 2xl:max-w-[1200px] xl:max-w-[900px] space-y-6 overflow-x-hidden ">
    {PortfolioData.map((portfolio: PortfolioDataType, index: number) => {
     return <PortfolioCard key={index} data={portfolio} />;
    })}
