be621ba80e1eecc2b2a8b84fa128f2a14e4a4e42
Mehmet Alperen Yedik <mehmetalperenyedik@gmail.com>
2025-09-24 19:17:19 +0300

    refactor: PersonelData güncellendi, üniversite bilgileri ve açıklama metni değiştirildi.

--- src/models/personel.data.ts
+++ src/models/personel.data.ts
@@ -1,18 +1,18 @@
 export const PersonelData = {
- landing: {
-  firstLine: "Hey! My name is",
-  secondLine: "Mehmet Alperen YEDİK",
-  thirdLine: "and I am a web developer",
-  fourthLine: "if you want to know more, scroll down.",
- },
- icon: "7K's",
- phone: "+90 546 434 60 08",
- mail: "mehmetalperenyedik@gmail.com",
- instagram: "https://www.instagram.com/alperenyedik/",
- linkedin: "https://www.linkedin.com/in/alperen7k/",
- github: "https://github.com/Alperen7K",
- universityYear: "2022 / 2026 - ",
- university: "Selcuk University Mechatronic Engineering",
- description:
-  "I’m Mehmet Alperen Yedik, a passionate Web Developer with a focus on both frontend and backend technologies. Proficient in React.js, Next.js, Node.js, and .NET Core, I build robust and dynamic applications. I’m also skilled in JavaScript, TypeScript, and C#, with a deep understanding of database systems like PostgreSQL and MongoDB. My toolkit includes Docker, Git, and Nginx, allowing me to deliver optimized, scalable solutions. Constantly pushing my boundaries, I embrace every new challenge as a chance to grow and innovate.",
+  landing: {
+    firstLine: "Hey! My name is",
+    secondLine: "Mehmet Alperen YEDİK",
+    thirdLine: "and I am a web developer",
+    fourthLine: "if you want to know more, scroll down.",
+  },
+  icon: "7K's",
+  phone: "+90 546 434 60 08",
+  mail: "mehmetalperenyedik@gmail.com",
+  instagram: "https://www.instagram.com/alperenyedik/",
+  linkedin: "https://www.linkedin.com/in/alperen7k/",
+  github: "https://github.com/Alperen7K",
+  universityYear: "2025 / 2029",
+  university: "Anadolu University Management Information Systems",
+  description:
+    "I’m Mehmet Alperen Yedik, a passionate Web Developer with a focus on both frontend and backend technologies. Proficient in React.js, Next.js, Node.js, and .NET Core, I build robust and dynamic applications. I’m also skilled in JavaScript, TypeScript, and C#, with a deep understanding of database systems like PostgreSQL and MongoDB. My toolkit includes Docker, Git, and Nginx, allowing me to deliver optimized, scalable solutions. Constantly pushing my boundaries, I embrace every new challenge as a chance to grow and innovate.",
 };
