2e8b3b9571667c8ecdc4f03f69004b68f392d7a5
Mehmet Alperen Yedik <mehmetalperenyedik@gmail.com>
2025-12-06 08:12:37 +0300

    feature: Yeni 'SinavBelgesi' bileşeni eklendi; PDF dosyası iframe içinde görüntülenmek üzere 'public' klasörüne yüklendi.

---
+++ src/app/sinav-belgesi/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function SinavBelgesi() {
+  return (
+    <iframe
+      loading="lazy"
+      className="w-full h-full z-[1000] fixed top-0 left-0"
+      src="/sinavgirisbelge.pdf#toolbar=1&navpanes=0&scrollbar=1"
+      title="CV"
+    ></iframe>
+  );
+}
